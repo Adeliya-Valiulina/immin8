5ca40fa50ae1d64b3d01660eefa6e8a150e98639
Adeliya-Valiulina <adeliavaliulina@gmail.com>
2024-10-15 23:15:00 +0300

    delete lenis

--- script.js
+++ script.js
@@ -84,14 +84,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // const lenis = new Lenis();
 
-  // function raf(time) {
-  //   lenis.raf(time);
-  //   requestAnimationFrame(raf);
-  // }
-
-  // requestAnimationFrame(raf);
 
   // const block4 = document.querySelector(".services-wrapper");
 
